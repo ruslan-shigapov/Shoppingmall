d856ebeb518d381117cbf7d3fe2418795c09dd15
Ruslan Shigapov <ilgamovich@gmail.com>
2024-08-02 11:27:43 +0300

    Added customized project

---
+++ Shoppingmall/Shoppingmall/App/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Shoppingmall
+//
+//  Created by Ruslan Shigapov on 02.08.2024.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [
+            UIApplication.LaunchOptionsKey: Any
+        ]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        guard let window else { return false }
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Shoppingmall/Shoppingmall/Views/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Shoppingmall
+//
+//  Created by Ruslan Shigapov on 02.08.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
